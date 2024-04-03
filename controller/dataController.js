910722506e6d801f1f0208d4116fad2aea0df27d
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-04-04 05:40:04 +0600

    Fetching Chart data only

--- controller/dataController.js
+++ controller/dataController.js
@@ -6,8 +6,8 @@ exports.hello = async (req, res) => {
 
 exports.getAllTickers = async (req, res) => {
   try {
-    const tickers = await Chart.findOne();
-    res.json({ tickers });
+    const tickers = await Chart.findOne().select('chart');
+    res.json(tickers.chart); // Send only the chart array in the response
   } catch (error) {
     console.error('Error retrieving tickers:', error);
     res.status(500).json({ error: 'Internal Server Error' });
