546aa5c1b654c32f78f55ff86ff115c3bde17c31
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-04-06 05:57:01 +0600

    Same Date Problem Fixed

--- controller/dataController.js
+++ controller/dataController.js
@@ -8,9 +8,27 @@ exports.hello = async (req, res) => {
 exports.getChart = async (req, res) => {
   try {
     const tickers = await Chart.findOne().select('chart');
-    res.json(tickers.chart); // Send only the chart array in the response
+    const chartData = tickers.chart;
+
+    // Create an object to store unique dates as keys
+    const uniqueDates = {};
+
+    // Filter out duplicate values based on distinct dates
+    const filteredChartData = chartData.filter((item) => {
+      // Extract date from the current item
+      const date = new Date(item.date).toISOString().slice(0, 10); // Extract YYYY-MM-DD
+      // Check if the date is already in the object
+      if (uniqueDates[date]) {
+        return false; // If duplicate, filter it out
+      }
+      // If not a duplicate, mark the date as seen in the object and keep the item
+      uniqueDates[date] = true;
+      return true;
+    });
+
+    res.json(filteredChartData); // Send the filtered chart data in the response
   } catch (error) {
-    console.error('Error retrieving tickers:', error);
+    console.error('Error retrieving chart data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
