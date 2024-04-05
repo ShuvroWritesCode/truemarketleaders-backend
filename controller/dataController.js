70bddf3feb055e7a8982839af5d9c844a04d001f
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-04-06 05:12:24 +0600

    Fetched Service Details

--- controller/dataController.js
+++ controller/dataController.js
@@ -5,7 +5,7 @@ exports.hello = async (req, res) => {
   res.json({ message: 'Hello world' });
 };
 
-exports.getAllTickers = async (req, res) => {
+exports.getChart = async (req, res) => {
   try {
     const tickers = await Chart.findOne().select('chart');
     res.json(tickers.chart); // Send only the chart array in the response
@@ -15,13 +15,33 @@ exports.getAllTickers = async (req, res) => {
   }
 };
 
-
 exports.getOneBreakout = async (req, res) => {
   try {
     const breakoutReport = await BreakoutReport.find();
-    res.json(breakoutReport); 
+    res.json(breakoutReport);
   } catch (error) {
     console.error('Error retrieving breakout:', error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.getAllTickersWithDetails = async (req, res) => {
+  try {
+    const tickers = await BreakoutReport.find(
+      {},
+      'ticker change close priceChange'
+    ); // Select specific fields
+
+    if (tickers.length > 0) {
+      const firstTicker = tickers[0];
+      console.log('First ticker:', firstTicker);
+    } else {
+      console.log('No tickers found');
+    }
+
+    res.status(200).json({ tickers });
+  } catch (error) {
+    console.error('Error retrieving tickers:', error);
+    res.status(500).json({ message: 'Error fetching tickers' });
   }
 };
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const dataController = require('../controller/dataController');
 
-router.get('/show', dataController.getAllTickers);
+router.get('/show', dataController.getChart);
 router.get('/breakoutReport', dataController.getOneBreakout);
-router.get('/hello', dataController.hello);
+// router.get('/ticker', dataController.getAllTickers);
+router.get('/details', dataController.getAllTickersWithDetails);
 module.exports = router;
