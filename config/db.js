bbf0167e53692d1bef87374ec683c7c9dc5a268f
Ash200042123 <ashfaq@iut-dhaka.edu>
2024-04-09 16:21:37 +0600

    mongo issue fixed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {});
+    const conn = await mongoose.connect('mongodb+srv://sarhancapital:temporaryPassword100!@marketterminalfinance.lnb53b1.mongodb.net/?retryWrites=true&w=majority', {});
     console.log('Connected to db');
   } catch (err) {
     console.log(err.message);
